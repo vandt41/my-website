bcac01dd79fd3e91c80220229ad923c41d0959f3
vandt41 <vandt41@gmail.com>
2026-04-22 04:54:29 -0700

    feat: add login form toggling

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleLoginForm() {
+    const form = document.getElementById("login-container");
+    if (form.style.display === "none" || form.style.display === "") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+        document.getElementById("login-container").style.display = "none";
+    }
+}
+
+function showHighlight() {
+    const highlightButton = document.getElementById("highlight_button");
+
+    highlightButton.innerHTML = `
+        <video src="cs2.mp4" width="500" controls autoplay muted loop></video>
+        <pre>mot trong hang ty highlights anh tung lam dc</pre>
+    `;
+}
+
