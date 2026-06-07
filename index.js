f6cb7d59178e01c9750799cfd126378a37a6e91a
van <vandt41@gmail.com>
2026-06-07 14:54:33 +0700

    add stuffs

--- index.js
+++ index.js
@@ -5,5 +5,20 @@
 document.getElementById(`myH1`).textContent = `Notebook`;
 document.getElementById(`myP`).textContent = `Hello, World!`;
 
-let x = 123;
+function rollDice() {
+    const numOfDice = document.getElementById(`numOfDice`).value;
+    const diceResult = document.getElementById(`diceResult`);
+    const diceImages = document.getElementById(`diceImages`);
+    const values = [];
+    const images = [];
+    for (let i = 0; i < 6; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+    }
+    for (let i = 0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random() * 6);
+        values.push(value);
+        images.push(`<img src="assest/dice/${value}.png"></img>`)
+
+    }
+}
 
